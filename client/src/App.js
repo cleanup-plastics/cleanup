b8100611a2282dad2a319f15ba06e98187b19896
Aylin Ünal <ayl.uenal@gmail.com>
2021-03-04 14:12:02 +0100

    add frontend routes to client/App.js and index.js and headings to event and create event

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route } from "react-router-dom";
+import CreateEvent from './components/CreateEvent';
+import Events from './components/Events';
+  
+  
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route exact path="/events" component={Events} />
+      <Route exact path="/events/create" component={CreateEvent} />
     </div>
   );
 }
--- client/src/components/CreateEvent.js
+++ client/src/components/CreateEvent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+// import CountriesDropdown from "./CountriesDropdown";
 
 class CreateEvent extends Component {
   state = {
@@ -7,7 +8,7 @@ class CreateEvent extends Component {
     imagePath: "",
     imageName: "",
     //use userID?
-    owner: userID,
+    //owner: userID,
     location: "",
     street: "",
     city: "",
@@ -22,71 +23,74 @@ class CreateEvent extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="title">Title: </label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={this.state.title}
-          onChange={this.handleChange}
-        />
+      <div>
+        <h1>Create a new Event!</h1>
 
-        <label htmlFor="description">Description: </label>
-        <textarea
-          name="description"
-          id="description"
-          cols="30"
-          rows="10"
-          value={this.state.description}
-          onChange={this.handleChange}
-        ></textarea>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
 
-        {/* Image --> how to name imagepath and imagename in state/cloudinary? */}
+          <label htmlFor="description">Description: </label>
+          <textarea
+            name="description"
+            id="description"
+            cols="30"
+            rows="10"
+            value={this.state.description}
+            onChange={this.handleChange}
+          ></textarea>
 
-        <label htmlFor="image">Image: </label>
-        <input
-          type="file"
-          id="image"
-          name="image"
-          value={this.state.image}
-          onChange={this.handleChange}
-        />
+          {/* Image --> how to name imagepath and imagename in state/cloudinary? */}
 
-        <label htmlFor="location">Name of location: </label>
-        <input
-          type="text"
-          id="location"
-          name="location"
-          value={this.state.location}
-          onChange={this.handleChange}
-        />
+          <label htmlFor="image">Image: </label>
+          <input
+            type="file"
+            id="image"
+            name="image"
+            value={this.state.image}
+            onChange={this.handleChange}
+          />
 
-        <label htmlFor="street">Street: </label>
-        <input
-          type="text"
-          id="street"
-          name="street"
-          value={this.state.street}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="city">City: </label>
-        <input
-          type="text"
-          id="city"
-          name="city"
-          value={this.state.city}
-          onChange={this.handleChange}
-        />
+          <label htmlFor="location">Name of location: </label>
+          <input
+            type="text"
+            id="location"
+            name="location"
+            value={this.state.location}
+            onChange={this.handleChange}
+          />
 
-        <label htmlFor="country">Country: </label>
-        
+          <label htmlFor="street">Street: </label>
+          <input
+            type="text"
+            id="street"
+            name="street"
+            value={this.state.street}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="city">City: </label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={this.state.city}
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="country">Country: </label>
+
           {/* refer to long list of option values in another component: */}
-          <CountriesDropdown />
-       
+          {/* <CountriesDropdown /> */}
 
-        <button type="submit">Post this event!</button>
-      </form>
+          <button type="submit">Post this event!</button>
+        </form>
+      </div>
     );
   }
 }
