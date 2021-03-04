6daf22946496c18bfc5863d671eaa429b8f4d28f
“Elvia <elvia.sicuro@gmail.com>
2021-03-04 17:53:41 +0100

    started authentication

---
+++ client/src/components/Signup.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { signup } from '../services/auth';
+
+export default class Signup extends Component {
+
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    message: ''
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { username, email, password } = this.state;
+    signup(username, email, password)
+      .then(user => {
+        if (user.message) {
+          this.setState({
+            message: user.message,
+            username: '',
+            email: '',
+            password: ''
+          })
+        } else {
+          // put the response from the server in the state of App.js
+          console.log(user)
+          this.props.setUser(user);
+          this.props.history.push('/events');
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Signup</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            id="username"
+          />
+          <label htmlFor="email">eMail: </label>
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            id="email"
+          />
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            id="password"
+          />
+          <button type="submit">Sign Up</button>
+          {this.state.message && (
+            <h3>{this.state.message}</h3>
+          )}
+        </form>
+      </div>
+    )
+  }
+}
