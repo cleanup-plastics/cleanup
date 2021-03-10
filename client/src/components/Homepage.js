86d6a8892d548a3425cb6a4cf95c59a729222593
Aylin Ünal <ayl.uenal@gmail.com>
2021-03-10 12:44:50 +0100

    adjust image size in carousel on homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -23,7 +23,7 @@ class Homepage extends Component {
   render() {
     const breakPoints = [
       { width: 1, itemsToShow: 1 },
-      { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+      { width: 550, itemsToShow: 2, itemsToScroll: 1 },
       { width: 768, itemsToShow: 3 },
       { width: 1200, itemsToShow: 4 },
     ];
